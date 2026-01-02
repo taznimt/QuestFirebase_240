6430bcbc0e974f7777e3199bdbfa63963a1ff257
taznimt <taznimanwar06@gmail.com>
2026-01-02 19:21:10 +0700

    feat: add DestinasiDetail object with navigation arguments

--- app/src/main/java/com/example/firebase/view/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/firebase/view/route/DestinasiDetail.kt
@@ -1,4 +1,10 @@
 package com.example.firebase.view.route
 
-object DestinasiDetail {
+import com.example.firebase.R
+
+object DestinasiDetail: DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
