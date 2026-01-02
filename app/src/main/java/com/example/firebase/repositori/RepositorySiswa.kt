9a9d2fc80561234ce22bf8373f450a9d908abc5e
taznimt <taznimanwar06@gmail.com>
2026-01-02 19:10:05 +0700

    feat: define RepositorySiswa interface with CRUD methods

--- app/src/main/java/com/example/firebase/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/firebase/repositori/RepositorySiswa.kt
@@ -1,2 +1,8 @@
 package com.example.firebase.repositori
 
+import com.example.firebase.modeldata.Siswa
+
+interface RepositorySiswa {
+    suspend fun getDataSiswa(): List<Siswa>
+    suspend fun postDataSiswa(siswa: Siswa)
+}
