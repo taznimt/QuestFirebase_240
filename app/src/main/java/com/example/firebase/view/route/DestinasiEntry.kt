44c97a0c0f4ee1e40ef1b199b15784b37efbc437
taznimt <taznimanwar06@gmail.com>
2026-01-02 19:23:46 +0700

    feat: add DestinasiEntry object for adding new student

--- app/src/main/java/com/example/firebase/view/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/firebase/view/route/DestinasiEntry.kt
@@ -1,4 +1,8 @@
 package com.example.firebase.view.route
 
-object DestinasiEntry {
+import com.example.firebase.R
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route =  "item_entry"
+    override val titleRes= R.string.entry_siswa
 }
