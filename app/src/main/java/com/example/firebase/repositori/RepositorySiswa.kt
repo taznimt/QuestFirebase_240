af6db41207268f29cbe5ce5d886d7c1a5f8003c2
taznimt <taznimanwar06@gmail.com>
2026-01-02 19:12:06 +0700

    feat: define RepositorySiswa interface with CRUD methods

--- app/src/main/java/com/example/firebase/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/firebase/repositori/RepositorySiswa.kt
@@ -1,8 +1,28 @@
 package com.example.firebase.repositori
 
 import com.example.firebase.modeldata.Siswa
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
 
 interface RepositorySiswa {
     suspend fun getDataSiswa(): List<Siswa>
     suspend fun postDataSiswa(siswa: Siswa)
 }
+class FirebaseRepositorySiswa: RepositorySiswa{
+    private val db = FirebaseFirestore.getInstance()
+    private val collection = db.collection("siswa")
+
+    override suspend fun getDataSiswa(): List<Siswa> {
+        return try {
+            collection.get().await().documents.map{ doc ->
+                Siswa(
+                    id = doc.getLong("id")?.toLong()?:0,
+                    nama = doc.getString("nama")?:"",
+                    alamat = doc.getString("alamat")?:"",
+                    telpon = doc.getString("telpon")?: ""
+                )
+            }
+        }catch (e: Exception){
+            emptyList()
+        }
+    }
