aff4cc980b5324a55a6a2413f6c2f3894309abb3
taznimt <taznimanwar06@gmail.com>
2026-01-02 19:19:15 +0700

    feat: implement NavHost routes for Home and Entry screens

--- app/src/main/java/com/example/firebase/view/controllNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/firebase/view/controllNavigasi/PetaNavigasi.kt
@@ -3,10 +3,34 @@ package com.example.firebase.view.controllNavigasi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import com.example.firebase.view.route.DestinasiDetail
+import com.example.firebase.view.route.DestinasiEntry
+import com.example.firebase.view.route.DestinasiHome
 
 @Composable
 fun DataSiswaApp(navController: NavHostController = rememberNavController(), modifier:
 Modifier = Modifier){
     HostNavigasi(navController = navController)
+}
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route,
+        modifier = Modifier ){
+        composable(DestinasiHome.route) {
+            HomeScreen(navigateToItemEntry = { navController.navigate(
+                DestinasiEntry
+                    .route) },
+                navigateToItemUpdate = {
+                    navController.navigate("${DestinasiDetail.route}/${it}")})
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route)
+            })
+        }
+    }
 }
