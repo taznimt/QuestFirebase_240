e655e275c299dd84bd46f95f85e5f1893c7f063c
taznimt <taznimanwar06@gmail.com>
2026-01-02 19:22:39 +0700

    feat: add DestinasiEdit object for student data update

--- app/src/main/java/com/example/firebase/view/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/firebase/view/route/DestinasiEdit.kt
@@ -1,4 +1,10 @@
 package com.example.firebase.view.route
 
-object DestinasiEdit {
+import com.example.firebase.R
+
+object DestinasiEdit: DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
