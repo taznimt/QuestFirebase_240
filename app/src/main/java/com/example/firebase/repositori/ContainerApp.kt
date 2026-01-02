14f1f9dbdb379d6f5f7e81f62415ffe2e5b238df
taznimt <taznimanwar06@gmail.com>
2026-01-02 19:06:40 +0700

    feat: implement Dependency Injection container for RepositorySiswa

--- app/src/main/java/com/example/firebase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/firebase/repositori/ContainerApp.kt
@@ -1,2 +1,11 @@
 package com.example.firebase.repositori
 
+interface ContainerApp {
+    val repositorySiswa: RepositorySiswa
+}
+
+class DefaultContainerApp: ContainerApp{
+    override val repositorySiswa: RepositorySiswa by lazy {
+        FirebaseRepositorySiswa()
+    }
+}
