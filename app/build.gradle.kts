3716c7a6c5e791e2d9e3cc373cbad1cc0a22b009
taznimt <taznimanwar06@gmail.com>
2026-01-02 19:31:37 +0700

    feat: implement EntrySiswa screen with form validation and Scaffold

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.lifecycle.runtime.compose)
     implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.material3)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
