6fdde9b56ec299d7116b008ea94281cf5f31b126
taznimt <taznimanwar06@gmail.com>
2026-01-02 19:24:43 +0700

    feat: add DestinasiHome object for main screen routing

--- app/src/main/java/com/example/firebase/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/firebase/view/route/DestinasiHome.kt
@@ -1,4 +1,8 @@
 package com.example.firebase.view.route
 
-object DestinasiHome {
+import com.example.firebase.R
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes= R.string.app_name
 }
