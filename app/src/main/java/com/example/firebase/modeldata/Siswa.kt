bd90ec07721f6af9d2c7e0d265b238695ea230f8
taznimt <taznimanwar06@gmail.com>
2026-01-02 19:04:30 +0700

    feat: add extension function to convert Siswa to UIStateSiswa

--- app/src/main/java/com/example/firebase/modeldata/Siswa.kt
+++ app/src/main/java/com/example/firebase/modeldata/Siswa.kt
@@ -20,3 +20,8 @@ data class UIStateSiswa(
     val detailSiswa: DetailSiswa = DetailSiswa(),
     val isEntryValid: Boolean = false
 )
+
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
