e8e73a1ee7437dd381571a1ab35a59031caa1b23
taznimt <taznimanwar06@gmail.com>
2026-01-08 14:21:50 +0700

    feat: initialize EditViewModel and fetch student data for editing

--- app/src/main/java/com/example/firebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/firebase/viewmodel/EditViewModel.kt
@@ -1,2 +1,28 @@
 package com.example.firebase.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.firebase.modeldata.UIStateSiswa
+import com.example.firebase.repositori.RepositorySiswa
+import com.example.firebase.view.route.DestinasiDetail
+
+class EditViewModel(savedStateHandle: SavedStateHandle, private val repositorySiswa:
+RepositorySiswa
+): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositorySiswa.getSatuSiswa(idSiswa)!!
+                .toUiStateSiswa(true)
+        }
+    }
+}
