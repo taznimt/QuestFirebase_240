5783b98c97d45fa7e3e57e143ffac663548ba170
taznimt <taznimanwar06@gmail.com>
2026-01-08 14:23:36 +0700

    feat: add input validation logic for student data

--- app/src/main/java/com/example/firebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/firebase/viewmodel/EditViewModel.kt
@@ -31,4 +31,9 @@ RepositorySiswa
             UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput
                 (detailSiswa))
     }
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
 }
