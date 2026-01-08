412a70bd6c0604fe0028c9d5e6621d557baa1a87
taznimt <taznimanwar06@gmail.com>
2026-01-08 14:04:15 +0700

    feat: add StatusUIDetail sealed interface for UI state

--- app/src/main/java/com/example/firebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/firebase/viewmodel/DetailViewModel.kt
@@ -1,2 +1,9 @@
 package com.example.firebase.viewmodel
 
+import com.example.firebase.modeldata.Siswa
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: Siswa?) : StatusUIDetail
+
+}
+
