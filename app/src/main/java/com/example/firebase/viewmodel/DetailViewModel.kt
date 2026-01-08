d305d824e8ea33623acf3c5a4e4e66c3d3df4b58
taznimt <taznimanwar06@gmail.com>
2026-01-08 14:05:20 +0700

    feat: add Error state to StatusUIDetail

--- app/src/main/java/com/example/firebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/firebase/viewmodel/DetailViewModel.kt
@@ -4,6 +4,7 @@ import com.example.firebase.modeldata.Siswa
 
 sealed interface StatusUIDetail {
     data class Success(val satusiswa: Siswa?) : StatusUIDetail
+    object Error : StatusUIDetail
 
 }
 
