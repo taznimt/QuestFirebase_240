c0e17c75c61352bd6026a9c0da2650b393106504
taznimt <taznimanwar06@gmail.com>
2026-01-08 14:07:28 +0700

    feat: initialize DetailViewModel with RepositorySiswa injection

--- app/src/main/java/com/example/firebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/firebase/viewmodel/DetailViewModel.kt
@@ -1,11 +1,20 @@
 package com.example.firebase.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.firebase.modeldata.Siswa
+import com.example.firebase.repositori.RepositorySiswa
 
 sealed interface StatusUIDetail {
     data class Success(val satusiswa: Siswa?) : StatusUIDetail
     object Error : StatusUIDetail
     object Loading : StatusUIDetail
+
+}
+
+class DetailViewModel(savedStateHandle: SavedStateHandle, private val repositorySiswa:
+RepositorySiswa
+): ViewModel() {
 
 }
 
